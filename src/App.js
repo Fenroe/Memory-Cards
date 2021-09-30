f9be940cbd0e6327c239f304e17dc4003a2f5736
Fenroe <scrambles9494@gmail.com>
2021-09-30 18:39:59 +0100

    chore: collecting assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+// needs a scoreboard to track current score
+// needs a best score that displays the highest value the scoreboard has ever had
+// needs 12 cards with different images on them and a board to place them on
+// cards need to be displayed in a random order whenever one is clicked
+// - function to randomise their order
+// - an onClick prop on the cards
+// - function should be called when component mounts 
+
+import cards from './assets/svg-cards.svg';
+
 function App() {
   return (
     <div>
+      <svg>
+        <use href={cards} />
+      </svg>
     </div>
   );
 }
